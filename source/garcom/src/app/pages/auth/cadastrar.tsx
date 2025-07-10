141c4b50183e48d01e21a8eb2e41934b1c8847f5
Lucas Francisco de Albuquerque Barbosa <luuinhasalbuquerque@gmail.com>
2025-07-10 17:51:47 +0000

    ajustes shadcn

---
+++ source/garcom/src/app/pages/auth/cadastrar.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-2xl font-bold mb-8">Exemplo de uso do Button do shadcn/ui</h1>
+      <div className="flex items-center justify-center gap-4">
+        <Button className="w-64" variant="default">
+          CADSAT
+        </Button>
+        <Button className="w-64" variant="outline">
+           Outline Button
+        </Button>
+        <Button className="w-64" variant="destructive">
+          Destructive Button
+        </Button>
+      </div>
+    </main>
+  )
+}
