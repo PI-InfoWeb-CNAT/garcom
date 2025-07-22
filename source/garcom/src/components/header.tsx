827b6fa7d26d877f911309eb9ebc413b5110aee5
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-07-22 17:30:07 +0000

    agora sim ta pronto a poha do header

--- source/garcom/src/components/header.tsx
+++ source/garcom/src/components/header.tsx
@@ -12,28 +12,45 @@ export function Header() {
   const [open, setOpen] = React.useState(false)
 
   return (
-    <div className="flex items-center justify-center absolute top-0  bg-[#F65C5C] w-full h-28 p-6">
+    <div className="flex items-center justify-center fixed top-0  bg-[#F65C5C] w-full h-28 p-6">
       <header className="flex items-center justify-between w-full rounded-full h-14 pl-10 pr-10 box-border bg-white text-[#E55F4B]">
         <h1 className="flex items-center align-center pt-px">
           <Image src="/logo.svg" alt="Logo" width={60} height={60} />
         </h1>
         <DropdownMenu>
-          <DropdownMenuTrigger className="md:hidden flex flex-col justify-center items-center w-10 h-10 l--100px" onClick={() => setOpen(!open)} aria-label="Menu">
-            
+          <DropdownMenuTrigger className="cursor-pointer md:hidden flex flex-col justify-center items-center w-10 h-10 l--100px" onClick={() => setOpen(!open)} aria-label="Menu">
             <span className="block w-6 h-0.5 bg-[#E55F4B] mb-1"></span>
             <span className="block w-6 h-0.5 bg-[#E55F4B] mb-1"></span>
             <span className="block w-6 h-0.5 bg-[#E55F4B]"></span>
           </DropdownMenuTrigger>
-          <DropdownMenuContent
-  className="w-48 bg-[#3D3D3D] text-white"
-  align="end"
+
+          <DropdownMenuContent className="w-65 bg-[#3D3D3D] text-white border-none p-0 m-0 rounded-2xl" align="end"
 >
             {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> 
             <DropdownMenuSeparator />*/}
-            <a href="#"><DropdownMenuItem className="cursor-pointer font-poppins text-[1em] font-medium"><FaHome className="text-white text-2xl" />Página principal</DropdownMenuItem></a>
-            <a href="#"><DropdownMenuItem className="cursor-pointer font-poppins text-[1em] font-medium"><FaBookOpen className="text-white text-2xl" />Cardápio</DropdownMenuItem></a>
-            <a href="#"><DropdownMenuItem className="cursor-pointer font-poppins text-[1em] font-medium"><IoMdPeople className="text-white text-2xl" /> Funcionarios</DropdownMenuItem></a>
-            <a href="#"><DropdownMenuItem className="cursor-pointer font-poppins text-[1em] font-medium"><CgProfile className="text-white text-2xl"/>Perfil</DropdownMenuItem></a>
+            <a href="#">
+  <DropdownMenuItem className="cursor-pointer max-h-48px p-3 pl-6 gap-3 font-poppins text-[19px] align-center font-medium hover:!bg-[#777777] hover:!text-white transition-all">
+    <FaHome className="text-white !size-[23px]" />Página principal
+  </DropdownMenuItem>
+</a>
+
+<a href="#">
+  <DropdownMenuItem className="cursor-pointer max-h-48px p-3 pl-6 gap-3 font-poppins text-[19px] align-center font-medium hover:!bg-[#777777] hover:!text-white transition-all">
+    <FaBookOpen className="text-white !size-[23px]" />Cardápio
+  </DropdownMenuItem>
+</a>
+
+<a href="#">
+  <DropdownMenuItem className="cursor-pointer max-h-48px p-3 pl-6 gap-3 font-poppins text-[19px] align-center font-medium hover:!bg-[#777777] hover:!text-white transition-all">
+    <IoMdPeople className="text-white !size-[23px]" /> Funcionarios
+  </DropdownMenuItem>
+</a>
+
+<a href="#">
+  <DropdownMenuItem className="cursor-pointer max-h-48px p-3 pl-6 gap-3 font-poppins text-[19px] align-center font-medium hover:!bg-[#777777] hover:!text-white transition-all">
+    <CgProfile className="text-white !size-[23px] hover:text-[#3D3D3D]"/>Perfil
+  </DropdownMenuItem>
+</a>
           </DropdownMenuContent>
         </DropdownMenu>
         
