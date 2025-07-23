328e4baab8da0a52370527703cce9136f94fc9ec
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-07-23 17:31:09 +0000

    header / main / footer

--- source/garcom/src/app/perfil/editar/page.tsx
+++ source/garcom/src/app/perfil/editar/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { Header } from "@/components/header";
-import Image from "next/image";
-import Link from "next/link";
+import { Footer } from "@/components/footer";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -19,11 +18,11 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 export default function editarPerfil() {
-    const tituloClass = 
-        "text-[23px] font-bold mb-6 text-[##E55F4B]";
+    const tituloClass =  "text-[23px] font-bold mb-6 text-[##E55F4B]";
 
-    const inputClass =
-        "font-poppins rounded-full bg-[#EFEFEF] text-[1em] font-medium text-[#9E9E9E] placeholder:text-[#9E9E9E] placeholder:font-poppins placeholder:font-medium placeholder:text-[1em]";
+    const inputClass = "font-poppins rounded-full bg-[#EFEFEF] text-[1em] font-medium text-[#9E9E9E] placeholder:text-[#9E9E9E] placeholder:font-poppins placeholder:font-medium placeholder:text-[1em]";
+    
+    const mainClass = "!pt-35 flex flex-col h-screen bg-white p-7 md:p-36 !pb-0"
     
     
     const [foto, setFoto] = useState<string | null>(null)
@@ -36,13 +35,13 @@ export default function editarPerfil() {
     return (
     <div>
       <Header />
-      <main className="!pt-40 sm:pt-28 flex flex-col h-screen bg-white p-6 sm:p-13">
+      <main className={mainClass}>
         <h1 className={tituloClass}>Editar Restaurante</h1>
-        <div>
-            <img src={foto || "/default-profile.png"} alt="Foto do perfil" className="w-24 h-24 rounded-full mb-4" />
+        <div className="flex flex-col items-center justify-center h-30 w-30">
+            <img src={foto || "/default-profile.png"} alt="Foto do perfil" className="w-30 h-30 rounded-full mb-4" />
             <input type="file" accept="image/*" onChange={mudarFoto} className="mb-4 hidden" id="foto-perfil" />
-            <label htmlFor="foto-perfil" className="cursor-pointer rounded-full bg-[##FFE3CF] h-30 w-30 flex align-center justify-center">
-                <MdOutlineEdit className="text-[#F65C5C] !size-[19px] absolute b-0 r-0" />
+            <label htmlFor="foto-perfil" className="cursor-pointer rounded-full absolute z-4 b-0 r-0 bg-[##FFE3CF] h-30 w-30 flex align-center justify-center">
+                <MdOutlineEdit className="text-[#F65C5C] !size-[25px] " />
 
             </label>
 
@@ -52,6 +51,7 @@ export default function editarPerfil() {
           placeholder="hello word"
         />
       </main>
+      <Footer />
     </div>
   )
 }
