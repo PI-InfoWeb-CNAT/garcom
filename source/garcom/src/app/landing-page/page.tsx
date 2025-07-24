a4874d586526a7b447f2d6cd0a025b7d9948b4b7
Izabel Alice <124362770+IzabelAlice@users.noreply.github.com>
2025-07-24 14:51:07 +0000

    cpommit

--- source/garcom/src/app/landing-page/page.tsx
+++ source/garcom/src/app/landing-page/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
+import  Cardzinho  from "@/components/Cardzinho";
 
 export default function LandingPage() {
   const mainClass = "!pt-35 flex flex-col h-screen bg-white p-7 md:p-36 !pb-0";
@@ -17,7 +18,7 @@ export default function LandingPage() {
               className="h-auto w-full object-contain"
             />
           </section>
-          <section className={mainClass}>
+          <section className={mainClass + ""}>
             <Image
               src="/garc]om2.png"
               alt="Imagem do garçom"
@@ -25,50 +26,53 @@ export default function LandingPage() {
               height={384.81}
               className="h-auto"
             />
-            <div>
-              <h2 className="font-poppins text-2xl font-semibold text-[#F65C5C]">
-                A gente entende a sua correria
-              </h2>
-              <p className="text-[#9E9E9E]">
-                Aqui é simples. Você abre o cardápio do restaurante, escolhe o
-                que quer, faz seu pedido e… pronto! O pessoal da cozinha recebe
-                na hora. Você paga quando receber, em dinheiro, cartão, pix ou
-                do jeito que combinar. Rápido, direto e sem dor de cabeça.
-              </p>
-            </div>
-            <div>
-              <Image
-                src="/agua_de_coco.svg"
-                alt="Ícone da água de coco"
-                width={29}
-                height={28.98}
-                className="h-auto"
-              />
-              <h3 className="font-poppins text-[#F65C5C] uppercase">
-                Qualidade supimpa
-              </h3>
-              <p className="text-[#9E9E9E]">
-                Parceiros de confiança para garantir que tudo chegue do jeitinho
-                que você espera.
-              </p>
-            </div>
-            <div>
-              <Image
-                src="/taça.svg"
-                alt="Ícone de taça"
-                width={24}
-                height={28.98}
-                className=""
-              />
-              <h3 className="text-[#F65C5C]">Uma experiência digna</h3>
-              <p className="text-[#9E9E9E]">
-                No fim das contas, o que a gente quer é te ouvir dizer: "Ô,
-                Garçom… capricha aí!"
-              </p>
+            <div className="grid grid-cols-1">
+              <div>
+                <h2 className="font-poppins text-2xl font-semibold text-[#F65C5C]">
+                  A gente entende a sua correria
+                </h2>
+                <p className="text-[#9E9E9E]">
+                  Aqui é simples. Você abre o cardápio do restaurante, escolhe o
+                  que quer, faz seu pedido e… pronto! O pessoal da cozinha recebe
+                  na hora. Você paga quando receber, em dinheiro, cartão, pix ou
+                  do jeito que combinar. Rápido, direto e sem dor de cabeça.
+                </p>
+              </div>
+              <div>
+                <Image
+                  src="/agua_de_coco.svg"
+                  alt="Ícone da água de coco"
+                  width={29}
+                  height={28.98}
+                  className="h-auto"
+                />
+                <h3 className="font-poppins text-[#F65C5C] uppercase">
+                  Qualidade supimpa
+                </h3>
+                <p className="text-[#9E9E9E]">
+                  Parceiros de confiança para garantir que tudo chegue do jeitinho
+                  que você espera.
+                </p>
+              </div>
+              <div>
+                <Image
+                  src="/taça.svg"
+                  alt="Ícone de taça"
+                  width={24}
+                  height={28.98}
+                  className=""
+                />
+                <h3 className="text-[#F65C5C]">Uma experiência digna</h3>
+                <p className="text-[#9E9E9E]">
+                  No fim das contas, o que a gente quer é te ouvir dizer: "Ô,
+                  Garçom… capricha aí!"
+                </p>
+              </div>
             </div>
           </section>
           <section>
             <h2>Como funciona?</h2>
+            <Cardzinho />
           </section>
         </main>
         <Footer />
---
+++ source/garcom/src/components/Cardzinho.tsx
@@ -0,0 +1,10 @@
+const Cardzinho = () => {
+  return (
+    <div className="w-40 h-40 bg-white shadow-lg rounded-lg p-4">
+      <h2>Cardzinho Component</h2>
+      <p>This is a simple card component.</p>
+    </div>
+  );
+}
+
+export default Cardzinho;
