f9003a8cc037293e593bc6a3258591842e33d4ce
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-07-26 19:42:57 +0000

    acabei o front do editar

--- source/garcom/src/app/perfil/editar/page.tsx
+++ source/garcom/src/app/perfil/editar/page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Eye, EyeOff } from "lucide-react";
+import { Switch } from "@/components/ui/switch"
 import { useState } from "react";
 import { MdOutlineEdit } from "react-icons/md";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
@@ -30,9 +31,10 @@ type FormData = z.infer<typeof schema>;
 
 export default function editarPerfil() {
     const tituloClass =  "text-[23px] font-bold mb-6 text-[#F65C5C]";
-    const inputClass = "font-poppins rounded-full bg-[#EFEFEF] text-[1em] font-medium text-[#9E9E9E] placeholder:text-[#9E9E9E] placeholder:font-poppins placeholder:font-medium placeholder:text-[1em]";
+    const inputClass = "rounded-full bg-[#EFEFEF] text-[1em] font-medium text-[#9E9E9E] placeholder:text-[#9E9E9E] placeholder:font-medium placeholder:text-[1em]";
     const mainClass = "!pt-35 flex flex-col min-h-screen bg-white p-7 md:p-36 !pb-0"
-    
+    const labelClass = "text-[20px] font-medium text-[#9E9E9E] mb-[7px]";
+
     const [fotoPerfil, setFotoPerfil] = useState<string | null>(null)
     const [fotoBanner, setFotoBanner] = useState<string | null>(null)
 
@@ -130,143 +132,129 @@ export default function editarPerfil() {
           </label>
         </div>
         </section>
-        <form onSubmit={handleSubmit(editar)} className="flex flex-col gap-4">
-        <section className="flex flex-col gap-4">
-          <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-              <AccordionTrigger className="cursor-pointer !no-underline"><h2 className={tituloClass}>Informações do Restaurante</h2></AccordionTrigger>
-              <AccordionContent className=" flex flex-col gap-6">
-                
-                  <div>
-                    <label className="text-[20px] font-medium text-[#9E9E9E]">Nome do restaurante</label>
-                    <Input className={inputClass} type="text" placeholder="Nome do Restaurante"/>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="text-[20px] font-medium text-[#9E9E9E]">Nome do restaurante</label>
-                    <textarea 
-                    onInput={(e) => {
-                    const el = e.target as HTMLTextAreaElement;
-                    el.style.height = 'auto';
-                    el.style.height = el.scrollHeight + 'px';}}
-                     className="font-poppins rounded-[18px] bg-[#EFEFEF] text-[1em] p-3 font-medium text-[#9E9E9E] placeholder:text-[#9E9E9E] placeholder:font-poppins placeholder:font-medium transition-all placeholder:text-[1em]"
-                     name="descricao" placeholder="Digite uma descrição"></textarea>
-                  </div>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">CNPJ</label>
-                      <Input className={inputClass} type="text" placeholder="Digite"/>
+        <form onSubmit={handleSubmit(editar)} className="grid grid-cols-3 gap-0 md:gap-10">
+          <div className="col-span-3 md:col-span-2">
+            <section className="flex flex-col">
+            <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger className="cursor-pointer !no-underline border-b rounded-none h-[60px]  border-[#D9D9D9] "><h2 className={tituloClass}>Informações do Restaurante</h2></AccordionTrigger>
+                <AccordionContent className=" flex flex-col mt-9 w-full gap-4">
+                    <div>
+                      <label className={labelClass}>Nome do restaurante</label>
+                      <Input className={inputClass} type="text" placeholder="Nome do Restaurante"/>
                     </div>
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">Email</label>
-                      <Input className={inputClass} type="text" placeholder="Digite"/>
+                    <div className="flex flex-col">
+                      <label className={labelClass}>Descrição</label>
+                      <textarea onInput={(e) => {const el = e.target as HTMLTextAreaElement; el.style.height = 'auto'; el.style.height = el.scrollHeight + 'px';}}
+                      className="font-poppins rounded-[18px] bg-[#EFEFEF] text-[1em] p-3 font-medium text-[#9E9E9E] placeholder:text-[#9E9E9E] placeholder:font-poppins placeholder:font-medium transition-all placeholder:text-[1em]"
+                      name="descricao" placeholder="Digite uma descrição"></textarea>
                     </div>
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">Senha Atual</label>
-                      <Input className={inputClass} type="password" placeholder="*********"/>
-                    </div>
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">Nova senha</label>
-                      <Input className={inputClass} type="password" placeholder="Digite sua nova senha"/>
-                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div className="w-full">
+                        <label className={labelClass}>CNPJ</label>
+                        <Input className={inputClass} type="text" placeholder="Digite"/>
+                      </div>
+                      <div className="w-full">
+                        <label className={labelClass}>Email</label>
+                        <Input className={inputClass} type="text" placeholder="Digite"/>
+                      </div>
+                      <div className="w-full h-fit">
+                        <label className={labelClass}>Senha Atual</label>
+                        <Input className={inputClass} type="password" placeholder="*********"/>
+                      </div>
+                      <div className="w-full h-fit">
+                        <label className={labelClass}>Nova senha</label>
+                        <Input className={inputClass} type="password" placeholder="Digite sua nova senha"/>
+                      </div>
 
-                  </div>
+                    </div>
+                    
                   
-                
-              </AccordionContent>
-            </AccordionItem>
-        </Accordion>
-        </section>
+                </AccordionContent>
+              </AccordionItem>
+          </Accordion>
+            </section>
 
-        <section className="flex flex-col">
-          <Accordion type="single" collapsible>
-          <AccordionItem value="item-2">
-              <AccordionTrigger className="cursor-pointer !no-underline"><h2 className={tituloClass}>Localização</h2></AccordionTrigger>
-              <AccordionContent>
-                  <div className="grid grid-cols-4 gap-6">
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">CEP</label>
-                      <Input className={inputClass} type="text" placeholder="Digite"/>
-                    </div>
-                    <div className="w-full h-fit col-span-2">
-                      <label className="text-[20px] font-medium text-[#9E9E9E] ">Logradouro</label>
-                      <Input className={inputClass} type="text" placeholder="Digite"/>
-                    </div>
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">Nº</label>
-                      <Input className={inputClass} type="type" placeholder="233"/>
-                    </div>
-                    <div className="w-full h-fit col-span-3">
-                      <label className="text-[20px] font-medium text-[#9E9E9E] ">Complemento</label>
-                      <Input className={inputClass} type="text" placeholder="Digite"/>
-                    </div>
-                    <div className="w-full h-fit">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">Bairro</label>
-                      <Input className={inputClass} type="text" placeholder="Digite"/>
-                    </div>
-                    <div className="w-full h-fit col-span-2">
-                      <label className="text-[20px] font-medium text-[#9E9E9E]">Cidade</label>
-                      <Input className={inputClass} type="text" placeholder="Natal rs"/>
-                    </div>
-                    <div className="w-full h-fit col-span-2">
-                      <label className="text-[20px] font-medium text-[#9E9E9E] ">Estado</label>
-                      <Input className={inputClass} type="text" placeholder="Rio grande do norte"/>
-                    </div>
-                  </div>          
-              </AccordionContent>
-            </AccordionItem>
-        </Accordion>
-        </section>
+            <section className="flex flex-col">
+            <Accordion type="single" collapsible>
+            <AccordionItem value="item-2">
+                <AccordionTrigger className="cursor-pointer !no-underline border-b rounded-none h-[60px]  border-[#D9D9D9] "><h2 className={tituloClass}>Localização</h2></AccordionTrigger>
+                <AccordionContent className="mt-9">
+                    <div className="grid grid-cols-4 gap-6">
+                      <div className="w-full h-fit">
+                        <label className={labelClass}>CEP</label>
+                        <Input className={inputClass} type="text" placeholder="Digite"/>
+                      </div>
+                      <div className="w-full h-fit col-span-2">
+                        <label className={labelClass}>Logradouro</label>
+                        <Input className={inputClass} type="text" placeholder="Digite"/>
+                      </div>
+                      <div className="w-full h-fit">
+                        <label className={labelClass}>Nº</label>
+                        <Input className={inputClass} type="type" placeholder="233"/>
+                      </div>
+                      <div className="w-full h-fit col-span-3">
+                        <label className={labelClass}>Complemento</label>
+                        <Input className={inputClass} type="text" placeholder="Digite"/>
+                      </div>
+                      <div className="w-full h-fit">
+                        <label className={labelClass}>Bairro</label>
+                        <Input className={inputClass} type="text" placeholder="Digite"/>
+                      </div>
+                      <div className="w-full h-fit col-span-2">
+                        <label className={labelClass}>Cidade</label>
+                        <Input className={inputClass} type="text" placeholder="Natal rs"/>
+                      </div>
+                      <div className="w-full h-fit col-span-2">
+                        <label className={labelClass}>Estado</label>
+                        <Input className={inputClass} type="text" placeholder="Rio grande do norte"/>
+                      </div>
+                    </div>          
+                </AccordionContent>
+              </AccordionItem>
+          </Accordion>
+            </section>
+          </div>
 
-        <section className="flex flex-col">
-          <Accordion type="single" collapsible>
-          <AccordionItem value="item-3">
-              <AccordionTrigger className="cursor-pointer !no-underline"><h2 className={tituloClass}>Horários de Funcionamento</h2></AccordionTrigger>
-              <AccordionContent>
-                    <div className="flex flex-col gap-4">
-                      {horarios.map((dia, index) => (
-                        <div key={dia.dia} className="flex items-center justify-between border-b pb-2">
-                          <span className="w-32 text-gray-700">{dia.dia}</span>
+          <section className="flex flex-col col-span-3 md:col-span-1 mb-10">
+            <Accordion type="single" collapsible>
+            <AccordionItem value="item-3">
+                <AccordionTrigger className="cursor-pointer !no-underline border-b rounded-none h-[60px]  border-[#D9D9D9] "><h2 className={tituloClass}>Horários de Funcionamento</h2></AccordionTrigger>
+                <AccordionContent className="mt-9">
+                      <div className="flex flex-col gap-4">
+                        {horarios.map((dia, index) => (
+                          <div key={dia.dia} className="flex items-center justify-between gap-2 mb-4">
+                            <span className="w-29 font-medium text-[15px]  text-[#9E9E9E]">{dia.dia}</span>
 
-                          <label className="flex items-center gap-2">
-                            <input
-                              type="checkbox"
-                              checked={dia.aberto}
-                              onChange={() => toggleDia(index)}
-                              className="accent-red-500 w-5 h-5"
-                            />
-                            <span className="text-sm text-gray-600">
-                              {dia.aberto ? "Aberto" : "Fechado"}
-                            </span>
-                          </label>
+                            <label className="flex items-center gap-2">
+                              <Switch checked={dia.aberto} onCheckedChange={() => toggleDia(index)}/>
+                              <span className="text-[#9E9E9E] font-medium text-[13px]"> {dia.aberto ? "Aberto" : "Fechado"}</span>
+                            </label>
+                            
 
-                          <div className="flex gap-2 items-center">
-                            <input
-                              type="time"
-                              value={dia.inicio}
-                              disabled={!dia.aberto}
-                              onChange={(e) => alterarHorario(index, "inicio", e.target.value)}
-                              className="bg-gray-100 px-2 py-1 rounded disabled:opacity-50"
-                            />
-                            <span className="text-gray-500">-</span>
-                            <input
-                              type="time"
-                              value={dia.fim}
-                              disabled={!dia.aberto}
-                              onChange={(e) => alterarHorario(index, "fim", e.target.value)}
-                              className="bg-gray-100 px-2 py-1 rounded disabled:opacity-50"
-                            />
+                            <div className="flex gap-2 items-center">
+                              <input type="time" value={dia.inicio} disabled={!dia.aberto}
+                                onChange={(e) => alterarHorario(index, "inicio", e.target.value)}
+                                className={`${inputClass} h-8 w-19 no-clock border-blocked p-[4px]`}
+                              />
+
+                              <span className="text-gray-500">-</span>
+                              <input type="time" value={dia.fim} disabled={!dia.aberto}
+                                onChange={(e) => alterarHorario(index, "fim", e.target.value)}
+                                className={`${inputClass} h-8 w-19 no-clock border-blocked p-[4px]`}
+                              />
+                            </div>
                           </div>
-                        </div>
-                      ))}
-                    </div>
-                  
-                  
-              </AccordionContent>
-            </AccordionItem>
-        </Accordion>
-        </section>
+                        ))}
+                      </div>
+                    
+                    
+                </AccordionContent>
+              </AccordionItem>
+          </Accordion>
+          </section>
 
-        <Button type="submit" variant="rosa">Salvar Alterações</Button>
+          <Button type="submit" variant="rosa">Salvar Alterações</Button>
         </form>
          
 
