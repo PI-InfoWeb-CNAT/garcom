f9e4a6cb9e1f3164506cfe7d6c796955261adda0
Lucas Francisco de Albuquerque Barbosa <luuinhasalbuquerque@gmail.com>
2025-07-29 13:15:00 +0000

    update logout-form

--- source/garcom/src/app/auth/components/logout-form.tsx
+++ source/garcom/src/app/auth/components/logout-form.tsx
@@ -4,22 +4,24 @@ import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 
 export default function LogoutForm() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const onSubmit = async () => {
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // evita reload da página
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
-          router.push("/auth/entrar"); 
+          router.push("/auth/entrar");
         },
       },
     });
   };
-    return (
-        <form action={onSubmit} method="POST">
-            <Button className="w-40" variant="laranja" type="submit">
-                Sair
-        </Button>
-        </form>
-    );
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Button className="w-40" variant="laranja" type="submit">
+        Sair
+      </Button>
+    </form>
+  );
 }
